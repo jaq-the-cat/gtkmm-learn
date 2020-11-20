581775ff94f598f3b0db07ec37d698fdfd16c8f3
jaq-cat <jaq.cat@protonmail.ch>
2020-11-20 15:37:47 -0300

    app

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #define HEIGHT 800
 
 int main(int argc, char *argv[]) {
-    Gtk::Main kit(argc, argv);
+    Gtk::Main app(argc, argv);
 
     Gtk::Window win;
     win.set_title("Hello, GTK!");
     win.set_resizable(false);
     win.set_default_size(WIDTH, HEIGHT);
 
-    Gtk::Main::run(win);
+    app.run(win);
     return 0;
 }
