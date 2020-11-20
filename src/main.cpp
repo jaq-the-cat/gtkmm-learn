48e6d98621e06c0dec6beae55e59a23336931e37
jaq-cat <jaq.cat@protonmail.ch>
2020-11-20 15:34:58 -0300

    def size

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <gtkmm-3.0/gtkmm.h>
 
+#define WIDTH 1200
+#define HEIGHT 800
+
 int main(int argc, char *argv[]) {
     Gtk::Main kit(argc, argv);
 
     Gtk::Window win;
     win.set_title("Hello, GTK!");
+    win.set_resizable(false);
+    win.set_default_size(WIDTH, HEIGHT);
+
     Gtk::Main::run(win);
     return 0;
 }
