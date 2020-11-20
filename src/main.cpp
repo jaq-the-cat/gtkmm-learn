b74876b77688f0f0249e2ac94a99a6d224479e15
jaq-cat <jaq.cat@protonmail.ch>
2020-11-20 11:54:15 -0300

    dependency hell :^)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <gtkmm-3.0/gtkmm.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    Gtk::Main kit(argc, argv);
+
+    Gtk::Window win;
+    win.set_title("Hello, GTK!");
+    Gtk::Main::run(win);
     return 0;
 }
